a3f3ca56633bf477e0be20987978d2be87b73134
Tejaswi0417 <132050445+Tejaswi0417@users.noreply.github.com>
2023-05-17 11:35:36 +0530

    Add files via upload

---
+++ tours.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Journey | Packages</title>
+<meta charset="utf-8">
+<link rel="icon" href="images/favicon.ico">
+<link rel="shortcut icon" href="images/favicon.ico">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/jquery.js"></script>
+<script src="js/jquery-migrate-1.1.1.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.jqtransform.js"></script>
+<script src="js/jquery.equalheights.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/jquery.ui.totop.js"></script>
+<script>
+$(window).load(function () {
+    $().UItoTop({
+        easingType: 'easeOutQuart'
+    });
+});
+$(function () {
+    $(".form1").jqTransform();
+});
+</script>
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<link rel="stylesheet" media="screen" href="css/ie.css">
+<![endif]-->
+</head>
+<body>
+<header>
+  <div class="container_12">
+    <div class="grid_12">
+      <h1><a href="index.php"><img src="images/logo.png" alt=""></a></h1>
+      <div class="clear"></div>
+    </div>
+    <div class="menu_block">
+      <nav>
+        <ul class="sf-menu">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="about.php">About</a>
+            <ul>
+              <li><a href="#">Agency</a></li>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Team</a></li>
+            </ul>
+          </li>
+          <li><a href="gallery.php">Gallery</a></li>
+          <li class="current"><a href="tours.php">Packages</a></li>
+          <li><a href="contacts.php">Contacts</a></li>
+        </ul>
+      </nav>
+      <div class="clear"></div>
+    </div>
+    <div class="clear"></div>
+  </div>
+</header>
+<div class="main">
+  <div class="content">
+    <div class="container_12">
+      <div class="grid_9">
+        <h3>Special Packeges</h3>
+        <div class="tours">
+          <div class="grid_4 alpha">
+            <div class="tour"> <img src="images/p1.jpg" alt="" class="img_inner fleft">
+              <div class="extra_wrapper">
+                <p class="text1">Alibag </p>
+                <p class="price">For 7 days 6 Night<span>From Rs 5000</span></p>
+                <p class="price">For 3 days 2 Night<span>From 3000</span></p>
+                <a href="#" class=""></a> </div>
+            </div>
+          </div>
+          <div class="grid_4 omega">
+            <div class="tour"> <img src="images/p2.jpg" alt="" class="img_inner fleft">
+              <div class="extra_wrapper">
+                <p class="text1">Lonavala </p>
+                <p class="price">For 8 days 7 Night <span>From Rs 6000</span></p>
+                <p class="price">For 4 days 3 Night <span>From Rs 4000</span></p>
+                <a href="#" class=""></a> </div>
+            </div>
+          </div>
+          <div class="clear"></div>
+          <div class="grid_4 alpha">
+            <div class="tour"> <img src="images/p3.jpg" alt="" class="img_inner fleft">
+              <div class="extra_wrapper">
+                <p class="text1">Pavna lake</p>
+                <p class="price">For 7 days 6 Night<span>From Rs 5000</span></p>
+                <p class="price">For 3 days 2 Night <span>From Rs 3000</span></p>
+                <a href="#" class=""></a> </div>
+            </div>
+          </div>
+          <div class="grid_4 omega">
+            <div class="tour"> <img src="images/p4.jpg" alt="" class="img_inner fleft">
+              <div class="extra_wrapper">
+                <p class="text1">Bhandardara</p>
+                <p class="price">For 4 days 3 Night <span>From Rs 5000</span></p>
+                <p class="price">For 3 days 2 Night <span>From Rs 4000</span></p>
+                <a href="#" class=""></a> </div>
+            </div>
+          </div>
+          <div class="clear"></div>
+          <div class="grid_4 alpha">
+            <div class="tour"> <img src="images/p5.jpg" alt="" class="img_inner fleft">
+              <div class="extra_wrapper">
+                <p class="text1">Pune </p>
+                <p class="price">For 7 days 6 Night <span>From Rs 9000</span></p>
+                <p class="price">For 4 days 3 Night<span>From Rs 5000</span></p>
+                <a href="#" class=""></a> </div>
+            </div>
+          </div>
+          <div class="grid_4 omega ">
+            <div class="tour"> <img src="images/p6.jpg" alt="" class="img_inner fleft">
+              <div class="extra_wrapper">
+                <p class="text1">Mahabaleshwar</p>
+                <p class="price">For 9 days 8 Night<span> From Rs 8000</span></p>
+                <p class="price">For 5 days 4 Night <span>From Rs 6000</span></p>
+                <a href="#" class=""></a> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="grid_3">
+        <h3>Browse Tours</h3>
+        <form method="post" id="form1" class="form1" action="#">
+          <label class="mb0"> <span>Browse by Tour Operator</span>
+            <select name="select">
+              <option value="">Browse by Tour Operator</option>
+              <option value="">...</option>
+            </select>
+          </label>
+          <div class="clear"></div>
+          <a onClick="document.getElementById('form1').submit()" href="#" class="btn"> Search</a>
+        </form>
+        <h3>Search Tours</h3>
+        <form method="post" id="form2" class="form1" action="#">
+          <label > <span><span class="col1">All Tour Operators</span><br>
+            Destination</span>
+            <select name="select">
+              <option value="">Any destination</option>
+              <option value="">...</option>
+            </select>
+          </label>
+          <label > <span>Departing</span>
+            <select name="select">
+              <option value="">Any departing</option>
+              <option value="">...</option>
+            </select>
+          </label>
+          <label> <span>Price</span>
+            <select name="select">
+              <option value="">Any price</option>
+              <option value="">...</option>
+            </select>
+          </label>
+          <label class="mb0"> <span>Days</span>
+            <select name="select">
+              <option value="">Any Days</option>
+              <option value="">...</option>
+            </select>
+          </label>
+          <div class="clear"></div>
+          <a onClick="document.getElementById('form2').submit()" href="#" class="btn"> Search</a>
+        </form>
+      </div>
+      <div class="clear"></div>
+    </div>
+  </div>
+  <div class="bottom_block">
+    <div class="container_12">
+      <div class="grid_2 prefix_2">
+        <ul>
+          <li><a href="#">FAQS Page</a></li>
+          <li><a href="#">People Say</a></li>
+        </ul>
+      </div>
+      <div class="grid_2">
+        <ul>
+          <li><a href="#">Useful links</a></li>
+          <li><a href="#">Partners</a></li>
+        </ul>
+      </div>
+      <div class="grid_2">
+        <ul>
+          <li><a href="#">Insurance</a></li>
+          <li><a href="#">Family Travel</a></li>
+        </ul>
+      </div>
+      <div class="grid_2">
+        <h4>Contact Us:</h4>
+        TEL: 1-800-234-5678<br>
+        <a href="#">info@demolink.org</a> </div>
+      <div class="clear"></div>
+    </div>
+  </div>
+</div>
+<footer>
+  <div class="container_12">
+    <div class="grid_12">
+      <div class="socials"> <a href="#"></a> <a href="#"></a> <a href="#"></a> <a href="#"></a> </div>
+      <div class="copy"> Journey &copy; 2045 | <a href="#">Privacy Policy</a> | Design by: <a href="http://www.templatemonster.com/">TemplateMonster.com</a> </div>
+    </div>
+    <div class="clear"></div>
+  </div>
+</footer>
+</body>
+</html>
